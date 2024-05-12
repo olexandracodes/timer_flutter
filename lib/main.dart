ef1f8baeff69d1fdbac6dd0de3cd99c5b46821c4
Alexandra <121388500+olexandracodes@users.noreply.github.com>
2024-05-12 22:29:16 +0300

    main

--- lib/main.dart
+++ lib/main.dart
@@ -4,33 +4,45 @@ import 'package:timer_flutter/data/localizations.dart';
 import 'package:timer_flutter/pages/home_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: const FirebaseOptions(
-        apiKey: "AIzaSyB9EQ4LkFdYutKx4baDUIEIJIwu_oe6lf8",
-        authDomain: "timer-ip-flutter.firebaseapp.com",
-        projectId: "timer-ip-flutter",
-        storageBucket: "timer-ip-flutter.appspot.com",
-        messagingSenderId: "199670623917",
-        appId: "1:199670623917:web:089b0ddae0bc6eda846024"),
-  );
+void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  Future<void> _initializeFirebase() async {
+    await Firebase.initializeApp(
+      options: const FirebaseOptions(
+        apiKey: "AIzaSyB9EQ4LkFdYutKx4baDUIEIJIwu_oe6lf8",
+        authDomain: "timer-ip-flutter.firebaseapp.com",
+        projectId: "timer-ip-flutter",
+        storageBucket: "timer-ip-flutter.appspot.com",
+        messagingSenderId: "199670623917",
+        appId: "1:199670623917:web:089b0ddae0bc6eda846024",
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      localizationsDelegates: [
-        S.delegate,
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-      ],
-      home: HomePage(),
+    return FutureBuilder(
+      future: _initializeFirebase(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          return const MaterialApp(
+            debugShowCheckedModeBanner: false,
+            localizationsDelegates: [
+              S.delegate,
+              GlobalMaterialLocalizations.delegate,
+              GlobalWidgetsLocalizations.delegate,
+            ],
+            home: HomePage(),
+          );
+        } else {
+          return const CircularProgressIndicator();
+        }
+      },
     );
   }
 }
