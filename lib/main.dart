da0b2dc4c4d8ba1cc84c78c5ab3bfabcf3f7d4f6
Alexandra <121388500+olexandracodes@users.noreply.github.com>
2024-05-12 21:00:12 +0300

    Firebase -

--- lib/main.dart
+++ lib/main.dart
@@ -6,17 +6,6 @@ import 'package:firebase_core/firebase_core.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: const FirebaseOptions(
-      apiKey: "AIzaSyCTOGyAABOAUI0NnoClAcHOtuRB_THzgQE",
-      authDomain: "timer-45eb5.firebaseapp.com",
-      databaseURL: "https://timer-45eb5-default-rtdb.europe-west1.firebasedatabase.app",
-      projectId: "timer-45eb5",
-      storageBucket: "timer-45eb5.appspot.com",
-      messagingSenderId: "898156769425",
-      appId: "1:898156769425:web:40dfedda512e5067708f30",
-    ),
-  );
   runApp(const MyApp());
 }
 
